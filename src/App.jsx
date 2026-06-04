d8e13fbe2e55deeac4a2e0c184a5d8e254a79e07
aminata-byte <aminatadiane175@gmail.com>
2026-06-04 19:58:41 +0000

    feat: configuration des routes et création des pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,40 @@
- 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DonatePage from "./pages/DonatePage";
+import DaarasPage from "./pages/DaarasPage";
+import BecomePartnerPage from "./pages/BecomePartnerPage";
+import PartnerLoginPage from "./pages/partner/PartnerLoginPage";
+import PartnerDashboardPage from "./pages/partner/PartnerDashboardPage";
+import SubmitOfferPage from "./pages/partner/SubmitOfferPage";
+import EnrolledTalibesPage from "./pages/partner/EnrolledTalibesPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Pages publiques */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/faire-un-don" element={<DonatePage />} />
+        <Route path="/daaras" element={<DaarasPage />} />
+        <Route path="/devenir-partenaire" element={<BecomePartnerPage />} />
+
+        {/* Espace partenaire */}
+        <Route path="/partenaire/login" element={<PartnerLoginPage />} />
+        <Route
+          path="/partenaire/dashboard"
+          element={<PartnerDashboardPage />}
+        />
+        <Route
+          path="/partenaire/soumettre-offre"
+          element={<SubmitOfferPage />}
+        />
+        <Route
+          path="/partenaire/talibe-inscrits"
+          element={<EnrolledTalibesPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
